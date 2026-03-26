882fbca989fc5fde83653bb6386991d561142238
suryamahesh2003 <140837165+suryamahesh2003@users.noreply.github.com>
2026-03-26 18:51:39 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const cors = require("cors");
+const fetch = require("node-fetch");
+
+const app = express();
+app.use(cors());
+
+app.get("/api/*", async (req, res) => {
+  const url = "https://api.mangadex.org/" + req.params[0];
+
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    res.json(data);
+  } catch (error) {
+    res.status(500).json({ error: "Failed" });
+  }
+});
+
+app.listen(process.env.PORT || 3000);
