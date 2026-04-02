fb507febe8972ab2a0ccac23cf546f3aa6fd39b9
suryamahesh2003 <140837165+suryamahesh2003@users.noreply.github.com>
2026-04-02 08:27:00 +0530

    Update server.js

--- server.js
+++ server.js
@@ -35,6 +35,7 @@ app.use((req, res) => {
 
 // ✅ start server
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+
+app.listen(PORT, "0.0.0.0", () => {
   console.log("Server running on port " + PORT);
 });
